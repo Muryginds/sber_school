70b6afe8c198f62a6bebc25c126b4c98a5f2c22b
Muryginds <muryginds@gmail.com>
2021-04-30 14:40:00 +0300

    project structure changed
    packages changed

--- 02_Cycles/src/main/java/com/company/Task_2.java
+++ 02_Cycles/src/main/java/com/company/Task_2.java
@@ -1,40 +1,52 @@
+package com.company;
+
 import java.util.Scanner;
 
 public class Task_2 {
   private static final Scanner scanner = new Scanner(System.in);
   private static final char MARKER = '*';
   private static final char EMPTY_SPACE = ' ';
+
   public static void main(String[] args) {
     int height = Integer.parseInt(scanner.nextLine());
     int width = Integer.parseInt(scanner.nextLine());
     String word = scanner.nextLine();
     int wordLength = word.length();
 
-    if (wordLength + 2 <= width) {
+    if (width >= wordLength + 2) {
       int middleHeight = height % 2 == 1? height / 2 : height / 2 - 1;
+      String lineSeparator = System.lineSeparator();
       StringBuilder stringBuilder = new StringBuilder();
       StringBuilder markerString = new StringBuilder()
-          .append(String.valueOf(MARKER).repeat(width));
+          .append(buildString(MARKER, width));
 
       for (int i = 0; i < height; i++) {
         if (i == 0 || i == height - 1) {
-          stringBuilder.append(markerString).append("\n");
+          stringBuilder.append(markerString).append(lineSeparator);
         } else if (i == middleHeight) {
           int h = (width - wordLength - 2)/2;
           stringBuilder.append(MARKER)
-              .append(String.valueOf(EMPTY_SPACE).repeat(h))
+              .append(buildString(EMPTY_SPACE, h))
               .append(word)
-              .append(String.valueOf(EMPTY_SPACE).repeat(width - wordLength - h - 2))
-              .append(MARKER).append("\n");
+              .append(buildString(EMPTY_SPACE, width - wordLength - h - 2))
+              .append(MARKER).append(lineSeparator);
         } else {
           stringBuilder.append(MARKER)
-              .append(String.valueOf(EMPTY_SPACE).repeat(width - 2))
-              .append(MARKER).append("\n");
+              .append(buildString(EMPTY_SPACE, width - 2))
+              .append(MARKER).append(lineSeparator);
         }
       }
       System.out.println(stringBuilder.toString());
     } else {
       System.out.println("Ошибка");
     }
+  }
+
+  private static StringBuilder buildString(char symbol, int num) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < num; i++) {
+      sb.append(symbol);
+    }
+    return sb;
   }
 }
